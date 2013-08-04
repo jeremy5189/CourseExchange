58d38a6fc8027019dac43d074029759bf52a1cc9
Jeremy Yen <jeremy5189@gmail.com>
2013-08-04 23:29:16 +0800

    Add Config Example and README

---
+++ include/config-example.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * -----------------------
+ *    Database Settings 
+ * -----------------------
+ */
+
+define( 'CONNECT_STR', "mysql:host=localhost;dbname=course_exchange" );
+define( 'DB_USER', '' );
+define( 'DB_PASS', '' );
+
+/* 
+ * -----------------------
+ *    Website Settings 
+ * -----------------------
+ */
+
+define( 'SITE_URL', 'http://localhost/clone/CourseExchange' );
+define( 'SITE_EMAIL' , 'noreply@example.com' );
+
